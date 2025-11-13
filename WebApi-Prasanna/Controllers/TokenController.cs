6c9461ec0a072200537c4772003746389bed5bf5
prasa <prasa@PRASANNA>
2025-11-13 15:29:00 +0530

    Token Generation

---
+++ WebApi-Prasanna/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TokenController : ControllerBase
+{
+    [HttpGet("generate")]
+    public IActionResult Generate()
+    {
+        var token = JwtTokenGenerator.GenerateToken(
+            secretKey: "super_secret_key_which_should_be_long",
+            issuer: "my-app",
+            audience: "my-users",
+            expiryMinutes: 60,
+            extraClaims: new Dictionary<string, string>
+            {
+                { "sub", "123" },
+                { "role", "admin" }
+            }
+        );
+
+        return Ok(new { token });
+    }
+}
